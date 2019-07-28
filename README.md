c09bb068b58c1e123bbfa18b1fcdeb0d39bc2f8f
andrey <dev@progital.io>
2019-07-28 18:35:13 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demo Site for gatsby-theme-wp-source-one
+
+Please refer to [gatsby-theme-wp-source-one](https://github.com/progital/gatsby-theme-wp-source-one) for the description.
---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `@progital/gatsby-theme-wp-source-one`,
+      options: {
+        wordPressUrl: `https://noh.progital.dev/`,
+        processPostTypes: [`Page`, `Post`, `Event`],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `WP Source Theme Demo`,
+        short_name: `WP Source`,
+        start_url: `/`,
+        background_color: `#919191`,
+        theme_color: `#919191`,
+        display: `standalone`,
+        icon: `src/images/favicon.jpg`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
